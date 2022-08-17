7522c57bebf70486bdb1c0ff715236e32699fb4c
kesel <a.kiselyoff@gmail.com>
2022-08-17 22:35:29 +0300

    exemple morgan

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hi');
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+// app.use(morgan('short'));
+// app.use(morgan('tiny'));
+
+app.get('/', function (req, res) {
+  res.send('hello, world!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
